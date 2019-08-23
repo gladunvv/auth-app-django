4863c937119a05b4e0595cfe45833bac65239406
Lancelot <bincha.1997@gmail.com>
2019-08-23 16:40:03 +0300

    url update

--- myapp/social_auth/urls.py
+++ myapp/social_auth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from social_auth.views import StartedScreenView, RequestFriends, LogoutView
 
 urlpatterns = [
-    path('', StartedScreenView.as_view(), name='starteds_screen'),
+    path('', StartedScreenView.as_view(), name='started_screen'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('friends', RequestFriends.as_view(), name='friends')
 ]
